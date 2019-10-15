051bcd71f76a7f5941c6bcf4cc990b875adf5d92
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-10-15 15:40:13 +0100

    simplify the interface to make it more accessible for all devices

--- javascript/indexController.js
+++ javascript/indexController.js
@@ -19,35 +19,28 @@ IndexController.prototype.buildTableBody = function(data) {
   var body = '<tbody class="govuk-table__body">';
   for(var i = 0; i < data.length; i++)
   {
+    var googleMapsURL = 'http://maps.google.com/?q='+ [
+      data[i]['Address line 1'],
+      data[i]['Address line 2'],
+      data[i]['Town / City'],
+      data[i]['County'],
+      data[i]['Postcode']
+    ].join(', ');
+
     body += '<tr class="govuk-table__row">';
-      /* CREATED TIMESTAMP */
+      /* LOCATION DETAILS */
       body += '<td class="govuk-table__cell">';
-        body += data[i]['Timestamp'];
-      body += '</td>';
-
-      /* ORGANISATION NAME */
-      body += '<td class="govuk-table__cell">';
-        body += data[i]['Organisation name(s)'];
-      body += '</td>';
-
-      /* ADDRESS */
-      body += '<td class="govuk-table__cell">';
-        body += '<strong>' + data[i]['Address line 1'] + '</strong><br/>';
+        body += '<a href="' + googleMapsURL + '"><h3 class="govuk-heading-s govuk-!-margin-top-3">' + data[i]['Organisation name(s)'] + '</h3></a>';
+        body +=  data[i]['Address line 1'] + '<br/>';
         if( data[i]['Address line 2'] ) body += data[i]['Address line 2'] + ', ';
         if( data[i]['Town / City'] ) body += data[i]['Town / City'] + ', ';
         if( data[i]['County'] ) body += data[i]['County'] + ', ';
         body += '<br/>' + data[i]['Postcode'];
         body += '<p class="govuk-body govuk-!-margin-top-5"><strong>Room location: </strong>' + data[i]['Where is the multi faith room located in the building? (For example 6th floor, opposite meeting room 612)'] + '</p>';
         if( data[i]['Any notes or things to be aware of?'] )  body += '<p class="govuk-body"><strong>Notes: </strong>' + data[i]['Any notes or things to be aware of?'] + '</p>';
-        
+        body += '<p class="govuk-body"><strong>Date added: </strong>' + data[i]['Timestamp'] + '</p>';
         // build the google maps query
-        body += '<p class="govuk-body govuk-!-margin-top-5"><a class="govuk-link" href="http://maps.google.com/?q='+ [
-          data[i]['Address line 1'],
-          data[i]['Address line 2'],
-          data[i]['Town / City'],
-          data[i]['County'],
-          data[i]['Postcode']
-        ].join(', ') +'" target="_blank">View on Google Maps</a></p>';
+        body += '<p class="govuk-body govuk-!-margin-top-5"><a class="govuk-link" href="'+ googleMapsURL +'" target="_blank">View on Google Maps</a></p>';
       body += '</td>'
     body += '</tr>'
   }
