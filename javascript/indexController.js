89e717cb7bc6a98763c380c43842bf7ff27b8eac
Dilwoar Hussain <dilwoar.hussain@digital.cabinet-office.gov.uk>
2019-10-24 10:48:59 +0100

    Add nunjucks templating

--- javascript/indexController.js
+++ javascript/indexController.js
@@ -2,6 +2,7 @@ function IndexController() {
   this.locationCSV = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTAFOpil_7wi0vIL3bki50rWRHbAJQ3pE7q_dbqDsXOz8OGX0O-HpErZJls7LTJaMzs4MAbvBwBf81s/pub?gid=905377423&single=true&output=csv";
   this.populateTable();
   this.initTableSearch();
+  nunjucks.configure('views', { autoescape: true });
 }
 
 /************************
@@ -13,8 +14,10 @@ IndexController.prototype.populateTable = function() {
     header: true,
     download: true,
     complete: function(results) {
-      var body = _this.buildTableBody(results.data);
-      document.querySelectorAll('#location-list tbody')[0].outerHTML = body;
+      _this.buildTableBody(
+        document.querySelectorAll('#location-list tbody')[0],
+        results.data
+      );
       _this.initLinkTracking();
     }
   });
@@ -23,63 +26,34 @@ IndexController.prototype.populateTable = function() {
 /*
  * @todo: Use a templating engine rather than JS
  */
-IndexController.prototype.buildTableBody = function(data) {
-  var body = '<tbody class="govuk-table__body">';
-  for(var i = 0; i < data.length; i++)
-  {
-    var _data = data[i];
-    if (!_data["Timestamp"].length || _data['Approved'] !== "TRUE") {
-      continue;
+IndexController.prototype.buildTableBody = function(tableBody, rooms) {
+  rooms = rooms.map(function(room){
+    if (!room["Timestamp"] || !room["Timestamp"].length || room['Approved'] !== "TRUE") {
+      return null;
     }
 
-    var googleMapsURL = 'http://maps.google.com/?q='+ [
-      _data['Address line 1'],
-      _data['Address line 2'],
-      _data['Town / City'],
-      _data['County'],
-      _data['Postcode']
-    ].join(', ');
+    // transformData
+    return {
+      createdAt:          room["Timestamp"],
+      organisationName:   room['Organisation name(s)'],
+      addressLine1:       room['Address line 1'],
+      addressLine2:       room['Address line 2'],
+      city:               room['Town / City'],
+      county:             room['County'],
+      postcode:           room['Postcode'],
+      location:           room['Where is the multi faith room located in the building? (For example 6th floor, opposite meeting room 612)'],
+      notes:              room['Any notes or things to be aware of?'],
+      googleMapsURL:      'http://maps.google.com/?q='+ [
+                            room.addressLine1,
+                            room.addressLine2,
+                            room.city,
+                            room.county,
+                            room.postcode
+                          ].join(', ')
+    };
+  }).filter( Boolean );
 
-    body += '<tr class="govuk-table__row">';
-      /* LOCATION DETAILS */
-      body += '<td class="govuk-table__cell">';
-        // organisation name
-        body += '<a href="' + googleMapsURL + '" target="_blank" class="govuk-link"><h3 class="govuk-heading-s govuk-!-margin-top-3">' + _data['Organisation name(s)'] + '</h3></a>';
-
-        // address
-        body +=  _data['Address line 1'] + '<br/>';
-        if( _data['Address line 2'] ) body += _data['Address line 2'] + ', ';
-        if( _data['Town / City'] ) body += _data['Town / City'] + ', ';
-        if( _data['County'] ) body += _data['County'] + ', ';
-        body += '<br/>' + _data['Postcode'];
-
-        // room location
-        body += '<p class="govuk-body govuk-!-margin-top-5"><strong>Room location: </strong>'
-            + _data['Where is the multi faith room located in the building? (For example 6th floor, opposite meeting room 612)']
-          + '</p>';
-
-        // notes
-        if( _data['Any notes or things to be aware of?'] )
-          body += '<p class="govuk-body"><strong>Notes: </strong>' + _data['Any notes or things to be aware of?'] + '</p>';
-
-        // date added
-        body += '<p class="govuk-body"><strong>Date added: </strong>' + _data['Timestamp'] + '</p>';
-
-        // build the google maps query
-        body += '<p class="govuk-body govuk-!-margin-top-5"><a class="govuk-link" href="'+ googleMapsURL +'" target="_blank">View on Google Maps</a></p>';
-      body += '</td>';
-    body += '</tr>';
-  }
-
-  // when no results
-  if (!data.length){
-    body += '<tr class="govuk-table__row">';
-      body += '<td class="govuk-table__cell" colspan="9">No data found</td>';
-    body += '</tr>'
-  }
-
-  body += '</tbody>';
-  return body;
+  tableBody.innerHTML = nunjucks.render('table-rows.html', { rooms: rooms });
 };
 
 IndexController.prototype.initLinkTracking = function() {
